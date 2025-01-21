119dd91c2cef707c3a009c65022c45fe66113be5
clshu <clshu2020@gmail.com>
2025-01-21 15:46:55 -0800

    Refactor the Header component

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Header() {
+  return (
+    <>
+      <img src="/logo.png" alt="A server suurounded by sparkles" />
+      <h1>Welcome to this NextJS course</h1>
+    </>
+  )
+}
